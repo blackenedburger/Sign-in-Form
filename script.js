d83249602224a5553b562ff0076869669e5a8914
blackenedburger <80690270+blackenedburger@users.noreply.github.com>
2023-11-19 00:29:31 +0100

    Add files via upload
    added pseudo styles

---
+++ script.js
@@ -0,0 +1,37 @@
+const formAll = document.querySelector("form");
+formAll.addEventListener('submit', (e) => {
+    //console.log(formAll.password);
+    //console.log(formAll.firstName);
+    console.log("hello");
+})
+
+
+
+
+// two red check marks by the passwords
+
+//connect the submit button so that it prints in the console.log to test it
+        // change the password type to posting it in console.log like in index12.html
+
+// check to see if the passwords are the same
+        // if same turn red x to green Checkmark 
+
+// when submit is pressed console.log the info
+        // if submit is pressed and they dont match an error message comes up
+
+
+//move button over
+
+// make input areas bigger for tablet media querie
+
+// fix the bottom grey area when it is resized
+
+// media queries to display login on the top picture on the bottom
+
+// media queries to fix it for tablets
+
+// fix the border shrinking from the top, maybe
+
+// make logo and logo strip
+
+//remove border colors
